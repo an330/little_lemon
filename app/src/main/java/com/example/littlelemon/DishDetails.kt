4ea49463319ab343b44d249976c4dcaa36443c8b
anup20.kumar <anupkk14198@gmail.com>
2025-02-12 00:52:27 +0530

    changes

--- app/src/main/java/com/example/littlelemon/DishDetails.kt
+++ app/src/main/java/com/example/littlelemon/DishDetails.kt
@@ -1,22 +1,47 @@
 package com.example.littlelemon
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ContentScale.Companion.FillWidth
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+
 
 @Composable
 fun DishDetails(id: Int) {
     val dish = requireNotNull(DishRepository.getDish(id))
+
     Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
-        //TODO: Insert code here
+        TopAppBar(title = { Text(text = "Dish Details") })
+
+        Image(
+            painter = painterResource(dish.imageResource),
+            contentDescription = "Dish image",
+            modifier = Modifier.fillMaxWidth(),
+            contentScale = ContentScale.FillWidth
+        )
+
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = dish.name, style = MaterialTheme.typography.h5)
+            Text(text = dish.description, style = MaterialTheme.typography.body1)
+            Counter()
+            Button(onClick = { /* Handle Add to Cart */ }) {
+                Text(text = stringResource(id = R.string.add_for, dish.price))
+            }
+        }
     }
 }
+
 
 @Composable
 fun Counter() {
